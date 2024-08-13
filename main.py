06f8352acd0393a40824b47c6f34499f2137dd29
Товарищ е. <146545075+Comrade-e@users.noreply.github.com>
2024-08-13 23:32:45 +0300

    Add files via upload
    added logging

--- main.py
+++ main.py
@@ -2,40 +2,63 @@ import asyncio
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+
+from aiogram.filters.exception import ExceptionTypeFilter
+from aiogram.types.error_event import ErrorEvent
+
+import mylogger
+import time
+
 
 import saver_reader as sr
 import ai
 from config import TOKEN
 
+lg = mylogger.MyLogger('info',f'{time.time()}.log')
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+@dp.error(ExceptionTypeFilter(Exception), F.update.message.as_("message"))
+async def handle_my_custom_exception(event: ErrorEvent, message):
+    lg.log('error', f'An error occured! {event.exception}')
+    await message.answer("Oops, something went wrong!")
+
 @dp.message(CommandStart())
 async def cmd_start(message):
+    lg.log('info', f'User {message.from_user.id} had started the bot.')
     await message.answer('Это эксперимент "Шизохрюша". /generate <n> - сгенерировать текст по промпту из n случайных '
                          'слов, уже существующих в текстах, хранящихся в памяти. /load <текст> - загрузить текст в память')
 
 @dp.message(Command('generate'))
 async def cmd_generate(message):
     amount = int(message.text.split(' ')[-1])
-    print(amount)
+    lg.log('info', f'User {message.from_user.id} requests to generate by {amount}')
     if amount > 350:
         await message.answer('Слишком много!')
     else:
         await message.answer('Генерация...')
-        text = await ai.gen('command-r+', 'Создайте эссе по ключевым словам на русском языке: ' + ' ,'.join(sr.randomwords(amount, 'data.txt')) + 'Текст должен быть тематически однороден')
+        text = await ai.gen('gpt-3.5-turbo', 'Создайте эссе по ключевым словам на русском языке: ' + ' ,'.join(sr.randomwords(amount, 'data.txt')) + 'Текст должен быть тематически однороден')
         await message.answer(text)
 
 @dp.message(Command('load'))
 async def cmd_load(message):
+    lg.log('info', f'User {message.from_user.id} loads the text.')
     txt = message.text.replace('/load', '')
     sr.save(txt, 'data.txt')
     await message.answer('Ваш текст принят.')
 
+@dp.message(F.text == '_error_')
+async def errortest(message):
+    await message.answer(str(1/0))
+
+
 
 async def main():
-    await dp.start_polling(bot)
+    try:
+        await dp.start_polling(bot)
+    except Exception as e:
+        lg.log('critical', f'Error! {str(e)}')
 
 
 if __name__ == '__main__':
---
+++ mylogger.py
@@ -0,0 +1,22 @@
+import logging
+import random
+
+class MyLogger():
+    LEVEL_MAP = {'debug': logging.DEBUG, 'info': logging.INFO, 'warning': logging.WARNING, 'error': logging.ERROR,
+                 'critical': logging.CRITICAL}
+    def __init__(self, level: str, filename: str):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        file_handler = logging.FileHandler(filename=filename, encoding='utf-8')
+        file_handler.setLevel(MyLogger.LEVEL_MAP[level])
+
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        file_handler.setFormatter(formatter)
+
+        self.logger.addHandler(logging.StreamHandler())
+        self.logger.addHandler(file_handler)
+        self.levels = {'debug': self.logger.debug, 'warning': self.logger.warning, 'info': self.logger.info,
+                       'error': self.logger.error, 'critical': self.logger.critical}
+
+    def log(self, level, text):
+        self.levels[level](text)
